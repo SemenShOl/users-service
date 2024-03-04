b32db29421b42c38a164c93d71a7afc35f0ad984
SemenShOl <semen.kursk.ru@gmail.com>
2024-03-04 12:59:33 +0300

    test commit

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,11 @@ import express from "express";
 const app = express();
 
 const port = 5000;
-let a;
 app.get("/", (req, res) => {
-  res.send("HI ,world");
+    res.send("HI ,world");
 });
 app.get("/user", (req, res) => {
-  res.send("This is user");
+    res.send("This is user");
 });
 
 app.listen(port, () => console.log(`Port ${port} is listening...`));
